66676272009543e79e4a0855f71bcd4f18cce9fd
Bohdan <Bohdan@DESKTOP-PIR5C7D>
2023-10-20 14:37:48 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication12/ConsoleApplication12.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+
+int main() {
+    int h, sum, n = 0;
+    std::cout << "Enter the number of H: ";
+    std::cin >> h;
+
+    std::cout << "Perfect numbers that are smaller than " << h << ": " << std::endl;
+    for (int i = 2; i < h; i++) 
+    {
+        sum = 1;
+        for (int j = 2; j < i; j++)
+        {
+            if (i % j == 0)
+            {
+                sum += j;
+            }
+        }            
+        if (sum == i)
+        {
+            std::cout << i << std::endl;
+            n++;
+        }
+    }
+    if (n == 0)
+        std::cout << "There are no perfect numbers";
+}
